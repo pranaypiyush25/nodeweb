d70e80c831d39f31c162ac56bd7f5f777725eb05
palakollu145 <83295582+palakollu145@users.noreply.github.com>
2022-07-28 13:12:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 // Constants
 const PORT = 5000;
-const HOST = 'localhost';
+
 
 // App
 const app = express();
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
   res.send('<h2 style="color: purple"> Java Home Docker App<h2>');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT);
+console.log(`Running on :${PORT}`);
